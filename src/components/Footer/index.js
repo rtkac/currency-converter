61b9dc3cefcba91b54ea632a5ad96e7cc31058f1
Radovan Tkáč <radovan.tkac@comsultia.com>
2019-09-08 19:21:02 +0200

    year date fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
+
+  const dateYear = () => {
+    let date = new Date();
+    return date.getFullYear();
+  }
+
   return (
     <footer className="text-center">
-      &copy; copyright 2019
+      &copy; copyright {dateYear()}
     </footer>
   )
 }
